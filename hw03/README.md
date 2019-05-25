366140d57e2430865e97895a72821ae4c8ee2352
hougrammer <david.m.hou@gmail.com>
2019-05-25 10:26:07 -0700

    lots

---
+++ hw03/README.md
@@ -0,0 +1,3 @@
+# Running Face Detector
+docker run -e DISPLAY=${DISPLAY} --rm --privileged -v /home/david/w251/hw03/:/hw03 --device /dev/video1 -it face
+
--- hw03/face_detect.py
+++ hw03/face_detect.py
@@ -1,21 +1,35 @@
 import numpy as np
-import cv2 as cv
+import cv2
 
-cap = cv.VideoCapture(1)
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(1)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+i = 0
 while(True):
+    i += 1
+    if not i % 60: print i
     ret, frame = cap.read()
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv.rectangle(gray, (x,y), (x+w,y+h), (255,0,0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        r = max(w, h) / 2
+        centerx = x + w / 2
+        centery = y + h / 2
+        nx = int(centerx - r)
+        ny = int(centery - r)
+        nr = int(r * 2)
 
-    cv.imshow('frame',gray)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+        faceimg = frame[ny:ny+nr, nx:nx+nr]
+        lastimg = cv2.resize(faceimg, (256, 256))
+        
+        cv2.imwrite("image.jpg", lastimg)
+
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-#cap.release()
-#cv.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
